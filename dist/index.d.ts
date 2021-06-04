7521b19c3d79014e1d3dbd29d617f64b0e3f08e2
Fedor Nezhivoi <f.nezhivoi@corp.vk.com>
2021-06-04 14:40:26 +0700

    updating for publish

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { FluentBundle } from '@fluent/bundle';
-declare class TurnslateLocalization {
-    bundles: Iterable<FluentBundle>;
-    constructor(bundles: Iterable<FluentBundle>);
-    getBundle(id: string): FluentBundle | null;
-}
-export declare const generateLocalization: (userLocales: string[], localizationConfig: Record<string, string>) => TurnslateLocalization;
-export {};
